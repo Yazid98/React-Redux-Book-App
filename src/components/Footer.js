a79215d5f9fd3b081af01cd0b1310a76ecb4024a
Yazid98 <ericbadarou@gmail.com>
2022-03-11 14:27:15 +0100

    inital commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="footer text-white bg-warning mt-5">
+            <div className="container p-3">
+                <span>&copy; TP React</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
